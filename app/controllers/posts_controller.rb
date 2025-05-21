7be02595226b19bd3cc06d4721db0cd2ee7f9743
scb8mp <scb8mp@gmail.com>
2025-05-21 21:05:35 +0000

    final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,20 +17,20 @@ class PostsController < ApplicationController
     render({ :template => "posts/show" })
   end
 
-  def create
-    the_post = Post.new
-    the_post.title = params.fetch("query_title")
-    the_post.body = params.fetch("query_body")
-    the_post.expires_on = params.fetch("query_expires_on")
-    the_post.board_id = params.fetch("query_board_id")
+ def create
+  the_post = Post.new
+  the_post.title = params.fetch("title")
+  the_post.body = params.fetch("body")
+  the_post.expires_on = params.fetch("expires_on")
+  the_post.board_id = params.fetch("board_id")
 
-    if the_post.valid?
-      the_post.save
-      redirect_to("/posts", { :notice => "Post created successfully." })
-    else
-      redirect_to("/posts", { :alert => the_post.errors.full_messages.to_sentence })
-    end
+  if the_post.valid?
+    the_post.save
+    redirect_to("/boards/#{the_post.board_id}", { :notice => "Post created successfully." })
+  else
+    redirect_to("/posts", { :alert => the_post.errors.full_messages.to_sentence })
   end
+end
 
   def update
     the_id = params.fetch("path_id")
